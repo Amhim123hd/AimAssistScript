f1eaf675812663a96562bd7e906cb507ff441039
Amhim123hd <153544801+Amhim123hd@users.noreply.github.com>
2025-01-25 13:15:49 -0500

    Update aim_assist.lua

--- aim_assist.lua
+++ aim_assist.lua
@@ -71,7 +71,7 @@ local function updateAimAssist(deltaTime)
         local targetPosition = CurrentTarget.Position
         local currentCameraDirection = Camera.CFrame.LookVector
         local desiredDirection = (targetPosition - Camera.CFrame.Position).Unit
-        local smoothSpeed = 0.050
+        local smoothSpeed = 0.5
 
         local newDirection = currentCameraDirection:Lerp(desiredDirection, smoothSpeed)
         Camera.CFrame = CFrame.new(Camera.CFrame.Position, Camera.CFrame.Position + newDirection)
